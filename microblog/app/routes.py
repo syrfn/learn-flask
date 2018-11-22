d6a6084cad2cbf01ae22462897839e8a5d3b0493
syarafina <farahiyahsyarafina@gmail.com>
2018-11-22 21:39:40 +0700

    looping

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,8 +1,20 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 
 def index():
     user = {'username':'Farah'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author':{'username':'John'},
+            'body':'Beautiful day in Portland'
+        },
+        {
+            'author':{'username':'Susan'},
+            'body':'The Avengers movie was cool!'
+        }
+    ]
+
+    return render_template('index.html', title='Home', user=user, posts=posts)
