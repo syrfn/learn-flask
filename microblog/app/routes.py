ac7233cdaf666ec42b75754b80e766944b5f3564
syarafina <farahiyahsyarafina@gmail.com>
2018-11-22 21:18:33 +0700

    render tempalte

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -4,14 +4,5 @@ from app import app
 @app.route('/index')
 
 def index():
-    user = {'username':'Miguel'}
-    return '''
-<html>
-    <head>
-        <title>Home Page - Microblog</title>
-    </head>
-    <body>
-        <h1>Hello, ''' + user['username'] + '''!</h1>
-    </body>
-</html>
-    '''
+    user = {'username':'Farah'}
+    return render_template('index.html', title='Home', user=user)
